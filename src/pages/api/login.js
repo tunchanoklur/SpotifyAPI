f626d983bfec9667a219e30fbad2eb492e25259a
kikk <kikpanda@gmail.com>
2019-09-02 16:04:51 +0700

    Retrieve returned token

--- src/pages/api/login.js
+++ src/pages/api/login.js
@@ -1,11 +1,15 @@
 const clientId = 'd57ab4103ced402191e4d0b305a3c241'
-const redirectURI = 'http://localhost:3000/'
+const redirectURI = 'http://localhost:3000/api/login'
 
 export default (req, res) => {
   res.setHeader('Content-Type', 'application/json')
   res.statusCode = 200
   var scopes = 'user-read-private user-read-email'
-
+  if (req.query.code) {
+    console.log(req.query.code)
+    res.redirect('http://localhost:3000/')
+    return
+  }
   res.redirect(
     'https://accounts.spotify.com/authorize' +
       '?response_type=code' +
